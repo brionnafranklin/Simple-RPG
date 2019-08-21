f09c2feaec86707a3844abcc24f8af27bd71708a
Brionna Franklin <s198017@students.aie.edu>
2019-08-21 09:52:46 -0500

    Created basic combat, including monster attack and flee

--- Simple RPG/Game.cs
+++ Simple RPG/Game.cs
@@ -10,7 +10,41 @@ namespace Simple_RPG
     {
         public void Start()
         {
-            Console.WriteLine("Welcome!");
+            String playerName = "";
+            int playerHealth = 100 ;
+
+            //Welcome the player
+            Console.Write("What is your name? ");
+            playerName = Console.ReadLine();
+            Console.WriteLine("Welcome, " +  playerName + ".");
+
+            //Monster encounter!
+            int monsterDamage = 13 ;
+            Console.WriteLine("");
+            Console.WriteLine("There is a monster in front of you!");
+
+            //player input
+            string action = "";
+            Console.Write("What will you do? (Flee/Fight) ");
+            action = Console.ReadLine();
+            if (action == "fight"  || action == "Fight")
+            {
+                //monster attack
+                Console.WriteLine("The monsters attacks! " + playerName + " takes " + monsterDamage + " damage! ");
+                playerHealth = playerHealth - monsterDamage;
+                Console.WriteLine(playerName + " has " + playerHealth + " health remaining. ");
+
+                //player attack
+                Console.WriteLine(playerName + " attacks! The monster is defeated! ");
+
+            }
+            else if (action == "flee" || action == "Flee")
+            {
+                //escape
+                Console.WriteLine("Got away Safely... ");
+
+            }
+
             Console.ReadKey();
         }
     }
